2e594f635f740130b11e8f05c84de87fe4639507
Vishnu Dixit <dixitvishnu57@gmail.com>
2025-09-18 15:49:57 +0530

    hero section

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all';
+import Hero from './sections/Hero';
+import Navbar from './sections/Navbar';
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+const App = () => {
+  return (
+   <main>
+    
+     <Navbar/>
+     <Hero/>
+
+   </main>
+  )
+}
+
+export default App
