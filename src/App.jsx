b2d5c3815e2da3c200790c89bce0c04b08a6bced
Vishnu Dixit <dixitvishnu57@gmail.com>
2025-09-18 16:23:33 +0530

    first video

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all';
 import Hero from './sections/Hero';
 import Navbar from './sections/Navbar';
+import FirstVideo from './sections/FirstVideo';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -10,9 +11,11 @@ gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
    <main>
-    
+
      <Navbar/>
      <Hero/>
+
+     <FirstVideo />
 
    </main>
   )
